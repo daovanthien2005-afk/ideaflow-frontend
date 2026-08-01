805828bf8f61ed415fe3af463207efc7f4bc2c8e
Đào Thiện <daovanthien2005@gmail.com>
2026-08-01 22:23:43 +0700

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
+const supabaseUrl = 'https://owiqgevrauwsrmnyqufi.supabase.co'
+const supabaseAnonKey = 'sb_publishable_zW1l7essvD2GahV819V-mA_XkjZQhh2' 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
